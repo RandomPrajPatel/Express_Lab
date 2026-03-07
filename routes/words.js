6f5e0fb824495ea5e98604ffb081119ab5619a3e
Praj Patel <patelpraj0210@gmail.com>
2026-03-06 23:58:24 -0500

    done

--- routes/words.js
+++ routes/words.js
@@ -7,20 +7,17 @@ router.get('/', (req, res) => {
     res.send('Word Home Page');
 });
 
-// Word of the Day route
 router.get('/wotd', async (req, res) => {
     const wordArray = await getWordFromDictionary();
     const [word, part, definition] = wordArray;
     res.render('wotd', { word, part, definition });
 });
 
-// All words route
 router.get('/allwords', async (req, res) => {
-    const allWords = await getAllWords(); // array of arrays
+    const allWords = await getAllWords(); 
     res.render('allwords', { words: allWords });
 });
 
-// Function to get a random word
 let getWordFromDictionary = async () => {
     try {
         const data = await readFile('resources/allwords.txt', 'utf8');
@@ -35,13 +32,12 @@ let getWordFromDictionary = async () => {
     }
 };
 
-// Function to get all words in alphabetical order
 let getAllWords = async () => {
     try {
         const data = await readFile('resources/allwords.txt', 'utf8');
         const lines = data.split('\n').filter(line => line.trim() !== '');
-        const wordList = lines.map(line => line.split('\t')); // array of arrays
-        wordList.sort((a, b) => a[0].localeCompare(b[0])); // sort by word
+        const wordList = lines.map(line => line.split('\t')); 
+        wordList.sort((a, b) => a[0].localeCompare(b[0])); 
         return wordList;
     } catch (err) {
         console.log("There was an error reading the file:", err);
